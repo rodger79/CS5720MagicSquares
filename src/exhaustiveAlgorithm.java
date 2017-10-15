1e5bc3e009ee090552de4fd881e819ba0be5c27d
Rodger Byrd <rodger.byrd@gmail.com>
2017-10-15 10:27:46 -0600

    generating all permutations of squares with diagonal checking

--- src/exhaustiveAlgorithm.java
+++ src/exhaustiveAlgorithm.java
@@ -6,12 +6,13 @@ import java.util.List;
 
 
 public class exhaustiveAlgorithm {
-	public static int size = 4;
+	public static int size = 3;
 	public static int square[][] = new int[size][size];
 	
 	public static void main(String[] args) {
 		
-
+		long startTime = System.currentTimeMillis();
+		
 		int squareSum = size*(size*size+1)/2;
 		//Integer[] values = new Integer[size*size];
 		List values1 = new ArrayList();
@@ -19,7 +20,7 @@ public class exhaustiveAlgorithm {
 		//populate values array with correct numbers
 		for (int i = 0; i < size*size ; i++){
 			values1.add(i+1);
-			System.out.println(java.util.Arrays.toString(values1.toArray()));
+			//System.out.println(java.util.Arrays.toString(values1.toArray()));
 		}
 		/*
 		//populate values array with correct numbers
@@ -32,7 +33,7 @@ public class exhaustiveAlgorithm {
 		//test each matrix to determine if it is a valid magic circle
 		
 		//option randomly shuffle to test other functions...
-		System.out.print("testing exhaustive search\n");
+	//	System.out.print("testing exhaustive search\n");
 		permute(values1,0);
 		
 		
@@ -94,6 +95,7 @@ public class exhaustiveAlgorithm {
 		
 		
 		System.out.print("test size 3\n");
+		System.out.print("magic number: " + 3*(3*3+1)/2 + "\n");
 		int squareMagic3[][] = new int[][]{
 				{2,7,6},
 				{9,5,1},
@@ -103,6 +105,7 @@ public class exhaustiveAlgorithm {
 			print(squareMagic3,3);
 		}
 		System.out.print("test size 4\n");
+		System.out.print("magic number: " + 4*(4*4+1)/2 + "\n");
 		int squareMagic4[][] = new int[][]{
 				{7,12,1,14},
 				{2,13,8,11},
@@ -113,7 +116,9 @@ public class exhaustiveAlgorithm {
 			print(squareMagic4,4);
 		}
 		
-		
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Total runtime = " + totalTime +"ms");
 	}
     static void permute(java.util.List<Integer> arr, int k){
         for(int i = k; i < arr.size(); i++){
@@ -167,16 +172,21 @@ public class exhaustiveAlgorithm {
 			}
 			if (vSum != squareSum) break;
 		}
-		
-		if (hSum == squareSum && vSum == squareSum) retval = true;
+		//check diagonal forwards
+		int dSum = 0;
+		for (int i= 0; i < size; i++){
+			dSum = dSum + square[i][i];
+		}
+		//check diagonal forwards
+		int d2Sum = 0;
+		int s2 = size-1;
+		for (int i= 0; i < size; i++){
+			d2Sum = d2Sum + square[i][s2];
+			s2--;
+		}
+		if (hSum == squareSum && vSum == squareSum && dSum == squareSum && d2Sum == squareSum) retval = true;
 		return retval;
 		
-	}
-	int[] permute(){
-		int[] retValues;
-		
-		
-		return retValues;
 	}
 	
 	//need function to print
